200e6741aa7099ae1eb18cc72325dbbb4130c81d
J <Mohammadiqbalramadhan21@gmail.com>
2024-01-17 20:30:10 +0700

    fog: track ViPER4AndroidFX repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,4 +21,7 @@ git clone https://github.com/KDEFFALT/fog-kernel -b artifact-new kernel/xiaomi/f
 
 # Vendor Sources
 git clone https://github.com/KDEFFALT/vendor_xiaomi_fog -b fourteen vendor/xiaomi/fog
+
+# Viper4AndroidFX
+git clone https://github.com/swiitch-OFF-Lab/packages_apps_ViPER4AndroidFX -n V4A packages/apps/ViPER4AndroidFX
 echo -e "${color}Finished sync!${end}"

e8b5243325f93b0bf8c4f156821a5a1717486607
Lunik <ishaandrej@gmail.com>
2024-01-19 16:30:04 +0000

    fog: Fix MTP QS
    Signed-off-by: Notkerd69 <29781314+Notkerd69@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -496,8 +496,8 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2.vendor:64
-
+    android.hardware.usb@1.0-service.basic
+	
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
